bd37afef489b9a026404e254ce19d982ed989050
D-Savel <dad.savel@gmail.com>
2021-03-24 17:19:33 +0100

    yarn init

--- cpCommand.js
+++ cpCommand.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 const readlineSync = require('readline-sync')
 
+readlineSync.question("Entrez le nom du répertoire?")
+
+readlineSync.question("Entrez le nom du fichier à copier ou à renommer?")
+
 
 fs.copyFileSync
 
